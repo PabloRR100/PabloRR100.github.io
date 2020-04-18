68537db675355f21a24acc7c24d478b92da917af
PabloRR100 <pabloruizruiz10@gmail.com>
2020-04-18 11:50:22 +0200

    Updates

---
+++ precache-manifest.9dfcfacd58cd4fa3bfc48a4705e147ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7283bfde9a990cf33f9301ac13babb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "076dae72f9905ab580d8",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "25e9b2fcd34bedcd0289",
+    "url": "/static/css/main.29eebac9.chunk.css"
+  },
+  {
+    "revision": "076dae72f9905ab580d8",
+    "url": "/static/js/2.3c91f894.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3c91f894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e9b2fcd34bedcd0289",
+    "url": "/static/js/main.c1f4e42a.chunk.js"
+  },
+  {
+    "revision": "57a0f55cac3770a9bdd5",
+    "url": "/static/js/runtime-main.322af556.js"
+  },
+  {
+    "revision": "b42740265ff91a20ae761b03193da2ad",
+    "url": "/static/media/linkedin.b4274026.png"
+  }
+]);
